b829fe65ddeedd817b426ab72de969888a375be8
Haftamu-S <haftamuSemere111@gmail.com>
2022-04-12 23:49:01 +0300

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,14 @@
  */
 int main(void)
 {
-	char main[] = "_putchar";
-	int i = 0;
-
-	while (main[i] != '\0')
-	{
-		_putchar(main[i]);
-		i++;
-	}
-	_putchar('\n');
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('w');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('/n');
+return(0);
 }
